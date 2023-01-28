d3180f8dffa1e0b967057c1b827639b8e024d6ac
Alexsander <alexsandercaac@gmail.com>
2023-01-28 17:39:06 -0300

    Added params.yaml
    Co-authored by: Pedro Chitarra <pedrochitarra@hotmail.com>

--- src/database_load.py
+++ src/database_load.py
@@ -3,8 +3,8 @@
     csv files.
 """
 
-from src.utils.data.load import load_table
-from src.utils.dvc.params import get_params
+from utils.data.load import load_table
+from utils.dvc.params import get_params
 import sqlite3
 
 
@@ -17,26 +17,26 @@ def main():
     sql_database = params["sql_database"]
     connection = sqlite3.connect(sql_database)
 
-    df_players = load_table("Player", connection)
-    df_players.to_csv("data/raw/players.csv")
+    df_player = load_table("Player", connection)
+    df_player.to_csv("data/interim/player.csv")
 
     df_country = load_table("Country", connection)
-    df_country.to_csv("data/raw/country.csv")
+    df_country.to_csv("data/interim/country.csv")
 
     df_league = load_table("League", connection)
-    df_league.to_csv("data/raw/league.csv")
+    df_league.to_csv("data/interim/league.csv")
 
     df_match = load_table("Match", connection)
-    df_match.to_csv("data/raw/df_match.csv")
+    df_match.to_csv("data/interim/match.csv")
 
     df_player_attributes = load_table("Player_Attributes", connection)
-    df_player_attributes.to_csv("data/raw/players_attributes.csv")
+    df_player_attributes.to_csv("data/interim/player_attributes.csv")
 
     df_team = load_table("Team", connection)
-    df_team.to_csv("data/raw/teams.csv")
+    df_team.to_csv("data/interim/team.csv")
 
     df_team_attributes = load_table("Team_Attributes", connection)
-    df_team_attributes.to_csv("data/raw/teams_attributes.csv")
+    df_team_attributes.to_csv("data/interim/team_attributes.csv")
 
 
 if __name__ == "__main__":
